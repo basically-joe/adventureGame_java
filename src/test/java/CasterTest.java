4b3ac0dacd6c40c1794409bd474775082c151ce3
Joe McColl <joe4kool@gmail.com>
2019-02-21 20:10:25 +0000

    canDamageTroll tests passing within casterTest

--- src/test/java/CasterTest.java
+++ src/test/java/CasterTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class CasterTest {
@@ -8,12 +10,19 @@ public class CasterTest {
 	Caster caster;
 	Spell spell;
 	Animal animal;
+	Troll troll;
+	ArrayList<ICharacter> enemies;
+	Weapon weapon2;
 
 	@Before
 	public void before(){
 		spell = new Spell("Doom", 40,2);
+		weapon2 = new Weapon("Club", 10);
 		animal = new Animal("Wyvern", 35);
 		caster = new Caster("Merlin", "Wizard", spell, animal);
+		troll = new Troll(100, weapon2);
+		enemies = new ArrayList<>();
+		enemies.add(troll);
 	}
 
 	@Test
@@ -49,6 +58,12 @@ public class CasterTest {
 	@Test
 	public void hasAnimal(){
 		assertEquals("Wyvern", caster.getAnimal().getName());
+	}
+
+	@Test
+	public void canDamageTroll(){
+		caster.use(enemies);
+		assertEquals(60, troll.getHP(), 0.01);
 	}
 
 }
