fd0669f3ea93c778971508bd30266a2611410374
Joe McColl <joe4kool@gmail.com>
2019-02-21 11:27:01 +0000

    Fighter class all tests passing

--- src/test/java/FighterTest.java
+++ src/test/java/FighterTest.java
@@ -26,4 +26,29 @@ public class FighterTest {
 		assertEquals("Barbarian", fighter.getType());
 	}
 
+	@Test
+	public void canGetHP(){
+		assertEquals(100, fighter.hp, 0.1);
+	}
+
+	@Test
+	public void canGetMP(){
+		assertEquals(100, fighter.mp, 0.1);
+	}
+
+	@Test
+	public void canGetXP(){
+		assertEquals(0, fighter.getXp(), 0.1);
+	}
+
+	@Test
+	public void canGetArmourName(){
+		assertEquals("Heavy", fighter.getArmour().getName());
+	}
+
+	@Test
+	public void canGetWeaponName(){
+		assertEquals("Deatheater", fighter.getWeapon().getName());
+	}
+
 }
